e487ec862461447a5fa7795750c245e8aad92b7c
Glaucia <glaucia.ap1258@gmail.com>
2021-11-24 21:23:35 -0300

    update

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,5 +1,4 @@
 const config = require('config.json');
-const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
 module.exports = db = {};
@@ -8,11 +7,14 @@ initialize();
 
 async function initialize() {
     const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const connectionString = (process.env.DATABASE_URL ? process.env.DATABASE_URL : `postgres://${user}:${password}@${host}:${port}/${database}`);
+    const connectionConfig = (process.env.NODE_ENV && process.env.NODE_ENV === "production") 
+                                ? ({dialect: 'postgres', ssl: true, dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }})
+                                : {dialect: 'postgres'};
 
+    const sequelize = new Sequelize(connectionString, connectionConfig);
+    
     db.User = require('../users/user.model')(sequelize);
     db.TypeUser = require('../typeUser/typeUser.model')(sequelize);
     db.Provider = require('../provider/provider.model')(sequelize);
@@ -27,7 +29,4 @@ async function initialize() {
     db.OrderRecipe = require('../order-recipe/order-recipe.model')(sequelize);
 
     await sequelize.sync();
-   
-    //await connection.query(`INSERT INTO kokimbosbackend.typeusers (id, nome, createdAt, updatedAt) VALUES ('1', '1', '2001-01-01', '2001-01-01');`);
-    //await connection.query(`INSERT INTO kokimbosbackend.users (id, nome, cargo, dataNascimento, cpf, rg, telefone, tipoUsuario, login, senha, hash, createdAt, updatedAt) VALUES ('1', 'Admin', 'Admin', '2000-01-01', '111.111.111-11', '22.222.222-2', '5555-5555', '1', 'admin', 'admin', '$2a$10$BnH.xm/PzRTKIszhjI.Wuuegi.OxfepEdheBAVV34GoiGBsy6KJRO', '2000-01-01', '2000-01-01');`);
 }
